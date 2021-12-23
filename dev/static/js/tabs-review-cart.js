abeda2cd6456c464328d61529956a41c3f246f6e
Artur Ballod <artur.ballod@gmail.com>
2021-12-24 01:06:12 +0200

    df

--- dev/static/js/tabs-review-cart.js
+++ dev/static/js/tabs-review-cart.js
@@ -38,3 +38,56 @@ function changeTabCart(evt, cartName) {
 }
 
 document.getElementById("defaultFilterOpen").click();
+
+
+var stepFirstBtn = document.querySelector('.js-step-first');
+var stepSecondTab = document.querySelector('.step-2');
+var stepSecondBtn = document.querySelector('.js-step-second');
+var stepThirdTab = document.querySelector('.step-3');
+var stepThirdBtn = document.querySelector('.js-step-three');
+var stepFourthTab = document.querySelector('.step-4');
+
+var cartFirstTab = document.querySelector('.cart-tab-1');
+var cartSecondTab = document.querySelector('.cart-tab-2');
+var cartThirdTab = document.querySelector('.cart-tab-3');
+var cartFourthTab = document.querySelector('.cart-tab-4');
+
+var secondTabWidth = stepSecondTab.offsetWidth;
+var thirdTabWidth = stepThirdTab.offsetWidth;
+var scrollSnapContainer = document.querySelector('.cart__filter-list');
+
+console.log(secondTabWidth + 40);
+
+if (stepFirstBtn) {
+    stepFirstBtn.addEventListener('click', function() {
+        event.preventDefault();
+        stepSecondTab.classList.remove('inactive');
+        stepSecondTab.click();
+        window.scrollTo(pageXOffset, 0);
+
+        cartFirstTab.classList.add('right');
+        cartSecondTab.classList.add('left');
+    });
+};
+
+if (stepSecondBtn) {
+    stepSecondBtn.addEventListener('click', function() {
+        event.preventDefault();
+        stepThirdTab.classList.remove('inactive');
+        stepThirdTab.click();
+        window.scrollTo(pageXOffset, 0);
+
+        cartFirstTab.classList.add('right2');
+    });
+};
+
+if (stepThirdBtn) {
+    stepThirdBtn.addEventListener('click', function() {
+        event.preventDefault();
+        stepFourthTab.classList.remove('inactive');
+        stepFourthTab.click();
+        window.scrollTo(pageXOffset, 0);
+
+        cartFirstTab.classList.add('right3');
+    });
+};
